8c55f8f5192dd203aa96c593f1ea8a32ca2a4947
Vedant Naidu <naiduvedant@gmail.com>
2023-02-20 18:14:06 +0530

    Edited Routes nane

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route exact path="/" element={<CreatorLinks />} />
         <Route exact path="/whyanidu" element={<CreatorLinks />} />
         <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="DashboardLinksNew" element={<DashboardLinksNew />} />
+          <Route path="home" element={<DashboardLinksNew />} />
           <Route path="Analytics" element={<Analytics />} />
         </Route>
       </Routes>
